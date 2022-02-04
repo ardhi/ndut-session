a3aba60526808e88532fffd5ffb39c997e802153
Ardhi Lukianto <ardhi@lukianto.com>
2022-02-04 20:11:23 +0700

    Session store

--- ndut/init.js
+++ ndut/init.js
@@ -1,3 +1,6 @@
-module.exports = async function (options) {
+const Store = require('../lib/store')
 
+module.exports = async function (options) {
+  const store = new Store({ scope: this })
+  options.store = store
 }
